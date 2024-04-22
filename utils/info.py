4bd873afdddd7c2a0feab6fe35b12b44a413fc4e
AreBhaai <160462350+AreBhaai@users.noreply.github.com>
2024-04-23 02:05:36 +0530

    Update info.py

--- utils/info.py
+++ utils/info.py
@@ -18,13 +18,13 @@
 
 import os
 
-API_ID       = int(os.environ.get("API_ID", ""))
-API_HASH     = os.environ.get("API_HASH", "")
-BOT_TOKEN    = os.environ.get("BOT_TOKEN", "")
-SESSION      = os.environ.get("SESSION", "")
+API_ID       = int(os.environ.get("API_ID", "20925335"))
+API_HASH     = os.environ.get("API_HASH", "bfd4023766d6081305379bdbe124e9c0")
+BOT_TOKEN    = os.environ.get("BOT_TOKEN", "7004285153:AAEOFBrxIlChXVoUwu9HQdP4_Q1OONTvZJA")
+SESSION      = os.environ.get("SESSION", "AQE_S5cAf_E9_A1rVIOyPks6728QlQCVguYTCPTdJlfCvR1oX1TX_Dd3_Y6e2aUtopgQcnUKT-gFixeg0UnFs2uUUeVxQJN_DABMBSPTobdgylnsl0xANUP82qwLXG7G9weSJPa2GvaVy-MjdmCwQhYXAaAL4XIusIYAflFsmH6r-xnuWLyg0jLzDkgKdxiRMRjjatzkHGrxfHOexN3H-PRKWYJLnJJ_D73U-HuAK13gP6N_LhaVl3UXcm2HhEFVWqGYt4JBAK-EnDaaf09jGgxSLmiKxnEcODXUCZd8FKS8swgcmo8gH9pxiGEeeDkF8cLEnGrlXMqdIyMtbtOrBDKUDI7OQwAAAAGFPEFwAA")
 TIME         = int(os.environ.get("TIME", 10))
-CHATS        = [int(cht) for cht in os.environ.get("CHATS", "").split()]
+CHATS        = [int(cht) for cht in os.environ.get("CHATS", "-1002130385569").split()]
 WHITE_LIST   = [int(wht) for wht in os.environ.get("WHITE_LIST", "").split()]
 BLACK_LIST   = [int(blk) for blk in os.environ.get("BLACK_LIST", "").split()]
-DATABASE_URI = os.environ.get("DATABASE_URI", "")
+DATABASE_URI = os.environ.get("DATABASE_URI", "mongodb+srv://knight_rider:GODGURU12345@knight.jm59gu9.mongodb.net/?retryWrites=true&w=majority")
 PORT         = os.environ.get("PORT", "8080")
